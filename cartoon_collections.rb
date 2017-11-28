c267ca22a2a1de8a1430031e44b506cf375697c1
Jonathan Luk <jluk87@gmail.com>
2017-11-28 00:09:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteers_calls)
   planeteers_calls.collect {|call| call.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 3}
 end
 
 def find_the_cheese# code an argument here
