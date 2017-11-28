17c7f2441b74c88bd6802d67be5693c25585bd04
Jonathan Luk <jluk87@gmail.com>
2017-11-28 00:07:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteers_calls)
-  planeteers.collect {|call| call.capitalize.exclamation}
+  planeteers_calls.collect {|call| call.capitalize+"!"}
 end
 
 def long_planeteer_calls# code an argument here
